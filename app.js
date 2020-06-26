8ae1141f27fdd1851335ea16104ae2c700ad3d6c
ahsan <ahsanjamil@live.com>
2020-06-27 01:01:14 +0500

    Calculator

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // JavaScript Document
- 	
+function numberGet(num) {
+  var display = document.getElementById('display');
+	display.value += num;
+}
+function reset() {
+  var display = document.getElementById('display');
+	display.value = ""
+}
+
+function result() {
+  var display = document.getElementById('display');
+	display.value = eval(display.value);
+}
